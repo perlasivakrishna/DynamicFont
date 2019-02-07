023d7c73fb799fd3768fe75e3d02ba328540d40b
sivakrishna <sivakrishna.perla@ymail.com>
2019-02-07 05:34:29 -0600

    FontsAdded

--- DynamicFont/ViewController.swift
+++ DynamicFont/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fontTestLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector (self.cotentSizeChange), name: UIContentSizeCategory.didChangeNotification, object: nil)
+        self.cotentSizeChange()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @objc func cotentSizeChange(){
+       // let font = UIFont.preferredFont(forTextStyle: .body)  use this directly if we are using sysem fonts  
+        let fontmetrics = UIFontMetrics(forTextStyle: .body)
+        let preferfont = fontmetrics.scaledFont(for: UIFont.SourceSansProBold(17))
+        fontTestLabel.font = preferfont
+    }
 
+    
 
 }
 
+extension UIFont {
+    
+    static func SourceSansProRegular(_ size:CGFloat) -> UIFont{
+        return UIFont(name: "SourceSansPro-Regular", size: size)!
+    }
+    
+    static func SourceSansProBold(_ size:CGFloat) -> UIFont{
+        return UIFont(name: "SourceSansPro-Bold", size: size)!
+    }
+    
+    static func SourceSansProSemiBold(_ size:CGFloat) -> UIFont{
+        return UIFont(name: "SourceSansPro-Semibold", size: size)!
+    }
+    
+}
