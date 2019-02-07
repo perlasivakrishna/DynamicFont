bc9368ef923ef392f481d244dfa3ea722309d33e
sivakrishna <sivakrishna.perla@ymail.com>
2019-02-07 03:50:35 -0600

    Initial Commit

---
+++ DynamicFont/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicFont
+//
+//  Created by Siva Krishna on 07/02/19.
+//  Copyright © 2019 Siva Krishna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
